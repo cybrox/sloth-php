49437c8b40a1fa91340aa5785ee884fcffdf4e06
Sven Marc Gehring <marc@cybrox.eu>
2014-12-23 07:54:02 +0100

    Added parameter class as request helper

---
+++ core/parameters.class.php
@@ -0,0 +1,46 @@
+<?php
+
+  class Parameters extends Base {
+
+    private static $get_dump;
+    private static $post_dump;
+
+
+    /**
+     * Get a parameter from the request arrays
+     * @param string $key - The key of the parameter
+     * @param string $type - The type of the parameter
+     */
+    public static function get($key, $type = null) {
+      if(self::$get_dump == null | self::$post_dump == null)
+        self::load_data();
+
+      switch (strtolower($type)) {
+        case 'post': return self::$post_dump[$key]; break;
+        case 'get':  return self::$get_dump[$key]; break;
+        default: return self::get_key($key); break;
+      }
+    }
+
+
+    /**
+     * Try to get the key value from either one of the arrays
+     * @param string $key - The key of the parameter
+     */
+    private static function get_key($key){
+      if(array_key_exists($key, self::$post_dump)) return self::$post_dump[$key];
+      if(array_key_exists($key, self::$get_dump)) return self::$get_dump[$key];
+      return null;
+    }
+
+
+    /**
+     * Load data from the request arrays into buffer
+     */
+    private static function load_data(){
+      self::$post_dump = $_POST;
+      self::$get_dump = $_GET;
+    }
+  }
+
+?>
